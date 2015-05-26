6f69f01e889fd259fb46cffb96bb554b4d34ed27
JKoty <jan.kotalik@email.cz>
2015-05-26 11:05:31 +0200

    První pokus o depsání dat k modelu.

--- netZkouska/netZkouska/Models/Student.cs
+++ netZkouska/netZkouska/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,19 @@ namespace netZkouska.Models
 		public int StudentID { get; set; }
 		public string name { get; set; }
 		public string surname { get; set; }
+
+		[NotMapped]
+		public string hasCourseCredit { get {
+			foreach (StudentWork sw in StudentWorks)
+			{
+				if (sw.Points > 45)
+				{
+					return "Má";
+				}
+			}
+			return "Nemá";
+		}
+		}
 
 		public virtual ICollection<Exam> Exams { get; set; }
 		public virtual ICollection<StudentWork> StudentWorks {get; set;}
