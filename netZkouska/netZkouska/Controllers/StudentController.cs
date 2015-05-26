33eb5910df11cd11908e956bb90193052887420f
JKoty <jan.kotalik@email.cz>
2015-05-26 15:41:30 +0200

    export do html

--- netZkouska/netZkouska/Controllers/StudentController.cs
+++ netZkouska/netZkouska/Controllers/StudentController.cs
@@ -8,121 +8,147 @@ using System.Web;
 using System.Web.Mvc;
 using netZkouska.DAL;
 using netZkouska.Models;
+using System.IO;
+using System.Text;
 
 namespace netZkouska.Controllers
 {
-    public class StudentController : Controller
-    {
-        private StudentNet db = new StudentNet();
+	public class StudentController : Controller
+	{
+		private StudentNet db = new StudentNet();
 
-        // GET: Student
-        public ActionResult Index()
-        {
-            return View(db.Students.ToList());
-        }
+		// GET: Student
+		public ActionResult Index()
+		{
+			//TextWriter tw = Response.Output;
+			//Response.Output = new StreamWriter("D:\\Documents\\Projekty\\output.html");
 
-        // GET: Student/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+			string myString = RenderRazorViewToString("~/Views/Student/Index.cshtml", View(db.Students.ToList()).Model);
 
-        // GET: Student/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+			System.IO.File.WriteAllText("D:\\Documents\\Projekty\\output.html", myString);
 
-        // POST: Student/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "StudentID,Name,Surname")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+			//Response.Output = tw;
 
-            return View(student);
-        }
+			return View(db.Students.ToList());
+		}
 
-        // GET: Student/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+		public string RenderRazorViewToString(string viewName, object model)
+		{
+			ViewData.Model = model;
+			using (var sw = new StringWriter())
+			{
+				var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext,
+																		 viewName);
+				var viewContext = new ViewContext(ControllerContext, viewResult.View,
+											 ViewData, TempData, sw);
+				viewResult.View.Render(viewContext, sw);
+				viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+				return sw.GetStringBuilder().ToString();
+			}
+		}
 
-        // POST: Student/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StudentID,Name,Surname")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
+		// GET: Student/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Student student = db.Students.Find(id);
+			if (student == null)
+			{
+				return HttpNotFound();
+			}
+			return View(student);
+		}
 
-        // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+		// GET: Student/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		// POST: Student/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "StudentID,Name,Surname")] Student student)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Students.Add(student);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+			return View(student);
+		}
+
+		// GET: Student/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Student student = db.Students.Find(id);
+			if (student == null)
+			{
+				return HttpNotFound();
+			}
+			return View(student);
+		}
+
+		// POST: Student/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "StudentID,Name,Surname")] Student student)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(student).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(student);
+		}
+
+		// GET: Student/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Student student = db.Students.Find(id);
+			if (student == null)
+			{
+				return HttpNotFound();
+			}
+			return View(student);
+		}
+
+		// POST: Student/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Student student = db.Students.Find(id);
+			db.Students.Remove(student);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
